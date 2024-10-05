506d1b88ccafcc1a3de6458015c093a44b52b7b8
mozaar07 <talha.mohd12@gmail.com>
2024-10-05 16:37:19 +0530

    Almost Completed
    Functions Implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
@@ -10,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "VAULT",
-  description: "Modern banking platform.",
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
